93c401522ebf8fc28ae6868a3ecf6cc7b4c18f58
MahbubHosssen <mhbabu2002@gmail.com>
2024-12-25 22:04:01 +0600

    create and use axios interceptor

--- src/hook/useAxiosSecure.jsx
+++ src/hook/useAxiosSecure.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:5000',
@@ -7,6 +10,27 @@ const axiosInstance = axios.create({
 })
 
 const useAxiosSecure = () => {
+    const {logOut} = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axiosInstance.interceptors.response.use(response => {
+            return response;
+        }, error => {
+            console.log('error interceptors', error)
+
+            if(error.status === 401 || error.status === 403){
+                // user logout
+                logOut()
+                .then(() => {
+                    navigate('/login')
+                })
+            }
+
+            return Promise.reject(error)
+        })
+    }, [logOut, navigate])
+
     return axiosInstance
 };
 
